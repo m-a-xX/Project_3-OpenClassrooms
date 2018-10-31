f3826e919ed7c62e0d84be005baecd5eb02637d5
max <maxence.vileo@orange.fr>
2018-10-31 01:36:59 +0100

    premieres idees 2

---
+++ constants.py
@@ -0,0 +1,9 @@
+#Window parameters
+nb_sprites = 15
+len_sprites =
+side_window = nb_sprites * len_sprites
+
+#Used pictures
+title = "pictures/title.png"
+macgyver = "pictures/MacGyver.png"
+guardian = "pictures/gardien.png"
--- game.py
+++ game.py
@@ -1,9 +1,30 @@
-"""Creation of the games's grid"""
-def create_grid(nb_lines, nb_colums):
+"""Jeu de lalyribthe
+Python3
+Uft8"""
+
+import pygame
+from pygame.locals import *
+
+from constants import *
+
+pygame.init()
+
+window = pygame.display.set_mode((side_window, side_window))
+
+icon = pygame.image.load(title)
+pygame.display.set_icon(icone)
+
+game = 1
+while game == 1:
+	
+
+
+
+	def create_grid(nb_lines, nb_colums):
 	grid = [[]] * nb_lines
 	for line in range(nb_lines):
 		grid[line] = [0] * nb_colums
 	return grid
 
-grid = create_grid(15, 15)
+	grid = create_grid(15, 15)
 
