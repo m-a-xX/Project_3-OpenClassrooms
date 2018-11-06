01db17c188313f657ff20dbe1588bc56f467c225
m-a-xX <maxence.vileo@orange.fr>
2018-11-06 17:40:30 +0100

    Base du jeu

--- classes.py
+++ classes.py
@@ -10,8 +10,6 @@ class Maze:
 	def __init__(self, file):
 		self.structure = 0
 		self.file = file
-
-	maze = 
  
 	def read(self):
 		with open(self.file, 'r') as file:
@@ -23,6 +21,8 @@ class Maze:
 				struct[line][column] = #char
 				if char == 2:
 					first_position = (line, column)
+				if char == 9:
+					guardian_position = (line, column)
 				column += 1
 			line += 1
 		
@@ -41,9 +41,14 @@ class Maze:
 					window.blit(macgyver, (line * len_sprites, column * len_sprites))					
 				column += 1
 			line += 1
+			pygame.display.flip()
 
-
-
+	@property
+	def isWall(self, pos_x, pos_y):
+		if struct[pos_x][pos_y] == 0:
+			return 1
+		else:
+			return 0
 
 """Manage characters and moves"""
 class Characters:
@@ -53,35 +58,46 @@ class Characters:
 		self.dep_y = dep_y
 		self.image = image
 
-	def firstPosition(self):
+	def firstPosition(self, (dep_x, dep_y)):
 		first position = ((dep_x * len_sprites), (dep_y * len_sprites))
 		return first_position
 
-	def isWall(self, pos_x, pos_y):
-		if struct[pos_x][pos_y] == 0:
+
+"""Child class of Characters for all specifics things to MacGyver"""
+class MacGyver(Characters):
+
+	def printMg(self, position):
+		window.blit(macgyver, (position))
+		pygame.display.flip()
+
+	def asleepGuardian(self, guardian_position, macgyver_position):
+		if guardian_position == macgyver_position and items_count == 3:
 			return 1
 		else:
 			return 0
 
-	def movements(self, pos_x, pos_y):
+	@property
+	def movements(self, (pos_x, pos_y)):
 		if event.type == KEYDOWN:
 			if event.key == K_LEFT:
 				pos_x -= 1
-				if isWall(pos_x, pos_y):
+				if isWall.Maze(pos_x, pos_y):
 					pos_x += 1
 			if event.key == K_RIGHT:
 				pos_x += 1
-				if isWall(pos_x, pos_y):
+				if isWall.Maze(pos_x, pos_y):
 					pos_x -= 1
 			if event.key == K_UP:
 				pos_y += 1
-				if isWall(pos_x, pos_y):
+				if isWall.Maze(pos_x, pos_y):
 					pos_y -= 1
 			if event.key == K_DOWN:
 				pos_y -= 1
-				if isWall(pos_x, pos_y):
+				if isWall.Maze(pos_x, pos_y):
 					pos_y += 1
-		return (pos_x * len_sprites, pos_y * len_sprites)
+		act_position = (pos_x * len_sprites, pos_y * len_sprites)
+		return act_position
+
 
 """Manage the items"""
 class Items:
@@ -89,6 +105,7 @@ class Items:
 	def __init__(self, image):
 		self.image = image
 
+	@property
 	def checkItems(self, position_item1, position_item2, position_item3, position_macgyver):
 		if position_macgyver == position_item1:
 			items_count += 1
@@ -98,6 +115,7 @@ class Items:
 			items_count += 1
 		return items_count
 
+	@property
 	def randomPosition(self, struct):
 		x = 0
 		y = 0
--- game.py
+++ game.py
@@ -7,8 +7,8 @@ import pygame
 from pygame.locals import *
 
 from constants import *
-#from classes import *
-#from display import *
+from classes import *
+from display import *
 
 pygame.init()
 
@@ -17,11 +17,33 @@ pygame.display.set_icon(title)
 
 """Principal loop"""
 game = 1
-while game == 1:
+while game:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			game = 0
 
+	"""Generate and print the maze"""
+	Maze.(grid)
+	print.Maze()
+
+	"""Generate and print items"""
+	item1_pos = randomPosition.Items(struct)
+	window.blit(item_1, (item1_pos))
+	item2_pos = randomPosition.Items(struct)
+	window.blit(item_2, (item2_pos))
+	item3_pos = randomPosition.Items(struct)
+	window.blit(item_3, (item3_pos))
+	pygame.display.flip()
+
+	"""Core of the game"""
+	win = 0
+	while not win:
+		act_position = first_position
+		movements.MacGyver(firstPosition.Characters(act_position))
+		printMg.MacGyver(act_position)
+		checkItems.Items(item1_pos, item2_pos, item3_pos, act_position)
+		if asleepGuardian.MacGyver(guardian_position, act_position):
+			win = 1
 
 
 
